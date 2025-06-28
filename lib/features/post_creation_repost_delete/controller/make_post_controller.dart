687f9cf5d4fe9be747988add9e94a24150e94295
salehakramsifat <salehakramsifat@gmail.com>
2025-06-28 16:51:05 +0600

    post screen changes

--- lib/features/post_creation_repost_delete/controller/make_post_controller.dart
+++ lib/features/post_creation_repost_delete/controller/make_post_controller.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
 
 class MakePostController extends GetxController {
-  final TextEditingController textController = TextEditingController();
+  @override
+  void onClose() {
+    textController.dispose();
+    super.onClose();
+  }
+
+  final textController = TextEditingController();
 
 
+  var picUpload = "".obs;
+  
+  void pickProfile() async {
+    final picker = ImagePicker();
+    final image = await picker.pickImage(source: ImageSource.gallery);
+    if (image != null) {
+      picUpload.value = image.path;
+    }
+  }
 }
