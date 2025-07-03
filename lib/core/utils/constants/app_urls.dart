a274e2329e83ce406520d39ad22a6f427c93906a
salehakramsifat <shakibhossainxstorm@gmail.com>
2025-07-03 10:41:40 +0600

    fetch all job, fulltime, part time, hybrit

--- lib/core/utils/constants/app_urls.dart
+++ lib/core/utils/constants/app_urls.dart
@@ -56,6 +56,7 @@ class AppUrls {
   static const String getProfileViewJob = '$_baseUrl/application/getProfileViewedJob';
   static const String getShortlistedJob = '$_baseUrl/application/getShortlistedJob';
   static const String getInterViewJob = '$_baseUrl/application/getInterviewJob';
+  static const String getSearchJobs = '$_baseUrl/job/all?name=Graphics Designer';
 
   static const String addFavorite = '$_baseUrl/favorite/add';
   static const String removeFavorite = '$_baseUrl/favorite/delete';
--- lib/features/search_notification_chat/controller/search_result_controller.dart
+++ lib/features/search_notification_chat/controller/search_result_controller.dart
@@ -1,34 +1,152 @@
-import 'package:flutter/cupertino.dart';
+// import 'package:flutter/cupertino.dart';
+// import 'package:get/get.dart';
+// import 'package:shuroo/features/search_notification_chat/data/model/search_result_model.dart' hide Jobs;
+//
+// import '../../../core/utils/constants/will_be_deleted.dart';
+// import '../../jobs/data/model/job_Model.dart';
+//
+// class SearchResultController extends GetxController{
+//
+//   final isFavorite = false.obs;
+//
+//   final search = TextEditingController();
+//
+//   final List<Jobs> jobsSystemsModel = [
+//     Jobs(
+//       id: 1,
+//       title: "💼  All Jobs",
+//     ),
+//     Jobs(
+//       id: 2,
+//       title: "🛠️  Full-time",
+//     ),
+//     Jobs(
+//       id: 3,
+//       title: "🕐  Part-time",
+//     ),
+//     Jobs(
+//       id: 4,
+//       title: "🏠 Hybrid",
+//     ),
+//   ];
+//
+//   final List<SearchResultModel> searchModelList = [
+//     SearchResultModel(
+//       id: 1,
+//       title: "UI/UX Designer",
+//       subTitle: "Wild World Conservation",
+//       fullTimeText: "Fulltime",
+//       remoteText: "Remote",
+//       outreachText: "Outreach",
+//       orText: "Portland, OR",
+//       applyText: "Apply Now",
+//       uiImagePath: WillBeDeleted.uiLogo,
+//     ),
+//     SearchResultModel(
+//       id: 2,
+//       title: "UI/UX Designer",
+//       subTitle: "Wild World Conservation",
+//       fullTimeText: "Fulltime",
+//       remoteText: "Remote",
+//       outreachText: "Outreach",
+//       orText: "Portland, OR",
+//       applyText: "Apply Now",
+//       uiImagePath: WillBeDeleted.ecoLogo,
+//     ),
+//     SearchResultModel(
+//       id: 3,
+//       title: "UI/UX Designer",
+//       subTitle: "Wild World Conservation",
+//       fullTimeText: "Fulltime",
+//       remoteText: "Remote",
+//       outreachText: "Outreach",
+//       orText: "Portland, OR",
+//       applyText: "Apply Now",
+//       uiImagePath: WillBeDeleted.uiLogo,
+//     ),
+//     SearchResultModel(
+//       id: 4,
+//       title: "UI/UX Designer",
+//       subTitle: "Wild World Conservation",
+//       fullTimeText: "Fulltime",
+//       remoteText: "Remote",
+//       outreachText: "Outreach",
+//       orText: "Portland, OR",
+//       applyText: "Apply Now",
+//       uiImagePath: WillBeDeleted.uiLogo,
+//     ),
+//     SearchResultModel(
+//       id: 5,
+//       title: "UI/UX Designer",
+//       subTitle: "Wild World Conservation",
+//       fullTimeText: "Fulltime",
+//       remoteText: "Remote",
+//       outreachText: "Outreach",
+//       orText: "Portland, OR",
+//       applyText: "Apply Now",
+//       uiImagePath: WillBeDeleted.ecoLogo,
+//     ),
+//     SearchResultModel(
+//       id: 6,
+//       title: "UI/UX Designer",
+//       subTitle: "Wild World Conservation",
+//       fullTimeText: "Fulltime",
+//       remoteText: "Remote",
+//       outreachText: "Outreach",
+//       orText: "Portland, OR",
+//       applyText: "Apply Now",
+//       uiImagePath: WillBeDeleted.uiLogo,
+//     ),
+//   ];
+//
+//   void toggleFavorite(int index) {
+//     searchModelList[index].isFavorite.toggle();
+//   }
+//
+//
+// }
+
+
+
+
+
+
+
+
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:shuroo/features/search_notification_chat/data/model/search_result_model.dart' hide Jobs;
+import 'package:shuroo/core/common/widgets/app_snackbar.dart';
+import 'package:shuroo/core/services/Auth_service.dart';
+import 'package:shuroo/core/services/network_caller.dart';
+import 'package:shuroo/core/utils/constants/app_urls.dart';
+import 'package:shuroo/features/jobs/data/model/get_all_jobs_model.dart';
+import 'package:shuroo/features/search_notification_chat/data/model/get_searched_jobs_model.dart' hide Datum;
 
 import '../../../core/utils/constants/will_be_deleted.dart';
-import '../../jobs/data/model/job_Model.dart';
+import '../data/model/search_result_model.dart';
 
-class SearchResultController extends GetxController{
+class SearchResultController extends GetxController {
 
-  final isFavorite = false.obs;
+  // variable
+  final isLoading = false.obs;
+  var selectedFilter = 'ALL JOB'.obs;
 
   final search = TextEditingController();
 
-  final List<Jobs> jobsSystemsModel = [
-    Jobs(
-      id: 1,
-      title: "💼  All Jobs",
-    ),
-    Jobs(
-      id: 2,
-      title: "🛠️  Full-time",
-    ),
-    Jobs(
-      id: 3,
-      title: "🕐  Part-time",
-    ),
-    Jobs(
-      id: 4,
-      title: "🏠 Hybrid",
-    ),
-  ];
+  // Model class
+  final getAllJob = GetAllJobsModel().obs;
+  final getSearchJobModel = GetSearchedJobsModel().obs;
+
+
+  void setFilter(String filter) {
+    selectedFilter.value = filter;
+  }
+
+  @override
+  void onInit() {
+    super.onInit();
+    fetchAllJobs();
+  }
 
   final List<SearchResultModel> searchModelList = [
     SearchResultModel(
@@ -104,21 +222,58 @@ class SearchResultController extends GetxController{
   }
 
 
+  // Added by Shahriar
+  Future<void> fetchAllJobs() async{
+    isLoading.value = true;
+    try{
+      final response = await NetworkCaller().getRequest(
+        AppUrls.getAllJobs,
+        token: "Bearer ${AuthService.token}"
+      );
+      if(response.isSuccess){
+        final data = response.responseData;
+        getAllJob.value = GetAllJobsModel.fromJson(data);
+      }else{
+        AppSnackBar.showError('Something Went Wrong!');
+      }
+    }catch(e){
+      AppSnackBar.showError(e.toString());
+    }finally{
+      isLoading.value = false;
+    }
+  }
+
+  // Added by Shahriar type handel
+  List<Datum> get filteredJobs {
+    if (getAllJob.value.data == null) return [];
+
+    if (selectedFilter.value == 'ALL JOB') {
+      return getAllJob.value.data!;
+    }
+
+    return getAllJob.value.data!
+        .where((job) => job.employmentType?.toUpperCase() == selectedFilter.value)
+        .toList();
+  }
+
+  // Added by Shahriar
+  Future<void> searchJobs(String searchValue) async{
+    isLoading.value = true;
+    try{
+      final response = await NetworkCaller().getRequest(
+        "${AppUrls.getSearchJobs}/$searchValue"
+      );
+      if(response.isSuccess){
+        final data = response.responseData;
+        getSearchJobModel.value = GetSearchedJobsModel.fromJson(data);
+      }else{
+        AppSnackBar.showError('Something Went Wrong!');
+      }
+    }catch(e){
+      AppSnackBar.showError(e.toString());
+    }finally{
+      isLoading.value = false;
+    }
+  }
+
 }
-
-
-
-
-
-
-
-//
-// import 'package:get/get.dart';
-//
-// class SearchResultController extends GetxController {
-//   var selectedFilter = 'All Jobs'.obs;
-//
-//   void setFilter(String filter) {
-//     selectedFilter.value = filter;
-//   }
-// }
