20ff6d44fe7d002541d8ae65f25afa1a3c8b128d
salehakramsifat <shakibhossainxstorm@gmail.com>
2025-07-05 15:57:03 +0600

    ..

--- lib/core/utils/constants/app_urls.dart
+++ lib/core/utils/constants/app_urls.dart
@@ -1,10 +1,11 @@
 class AppUrls {
   AppUrls._();
 
-  //static const String _baseUrl = 'http://10.0.20.12:6042/api/v1';
+  // Local 
+  static const String _baseUrl = 'http://10.0.20.12:6042/api/v1';
 
   // VPS Server ===========================
-  static const String _baseUrl = 'http://31.97.145.131:6042/api/v1';
+ // static const String _baseUrl = 'http://31.97.145.131:6042/api/v1';
 
   static const String signUp = '$_baseUrl/user/create';
   static const String login = '$_baseUrl/auth/login';
