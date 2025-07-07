962466971b2738437e3c2bd3af5d42287d6d5c7b
Nifat130 <mahingreen130@gmail.com>
2025-07-07 18:24:16 +0600

    edit post done by Nifat

--- lib/features/post_creation_repost_delete/controller/edit_post_controller.dart
+++ lib/features/post_creation_repost_delete/controller/edit_post_controller.dart
@@ -1,12 +1,17 @@
 
+import 'dart:convert';
 import 'dart:developer';
 
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+import 'package:http_parser/http_parser.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:mime/mime.dart';
 import 'package:shuroo/features/profile/controller/personal_creation_controller.dart';
 
 import '../../../core/common/widgets/app_snackbar.dart';
+import '../../../core/common/widgets/progress_indicator.dart';
 import '../../../core/services/Auth_service.dart';
 import '../../../core/services/network_caller.dart';
 import '../../../core/utils/constants/app_urls.dart';
@@ -20,7 +25,7 @@ class EditPostController extends GetxController{
   final captionController = TextEditingController();
   var picUploads = <String>[].obs;
 
-  RxBool isPostButtonEnabled = false.obs;
+  RxBool isPostButtonEnabled = true.obs;
 
   // @override
   // void onInit() {
@@ -57,6 +62,7 @@ class EditPostController extends GetxController{
     super.onInit();
     if(Get.arguments != null){
       isLoading.value = true;
+      postId.value = Get.arguments;
       controllerOne = Get.find<PersonalCreationController>();
       await getSinglePost(Get.arguments);
       log("===================================================================");
@@ -93,6 +99,66 @@ class EditPostController extends GetxController{
       log('Something went Wrong $e');
     } finally {
       //  hideProgressIndicator();
+    }
+  }
+
+  void updatePost() async{
+
+    final requestBody = {
+      "content": captionController.text
+    };
+
+    await requestToEditPost(requestBody, picUploads);
+  }
+  Future<void> requestToEditPost(Map<String, dynamic> requestBody, List<String> photoList) async{
+
+    try{
+      log("-----------------I am here in post now repo----------------------");
+      var request = http.MultipartRequest("PUT", Uri.parse(AppUrls.editPostById(postId)));
+
+      request.headers.addAll({'Authorization': "Bearer ${AuthService.token}"});
+
+      request.fields['bodyData'] = jsonEncode(requestBody);
+
+      for (int i = 0; i < photoList.length; i++) {
+        final path = photoList[i];
+
+        if (path.isNotEmpty) {
+          final mimeType = lookupMimeType(path) ?? "image/jpeg";
+          final splitMime = mimeType.split('/');
+
+          final file = await http.MultipartFile.fromPath(
+            "serviceVideo", // same field name for each image
+            path,
+            contentType: MediaType(splitMime[0], splitMime[1]),
+          );
+
+          request.files.add(file);
+        }
+      }
+      log("-----------------I am here in post now repo 2----------------------");
+      await showProgressIndicator();
+      final response = await request.send();
+      log(response.statusCode.toString());
+      log("-----------------I am here in post now after request----------------------");
+      if(response.statusCode == 200 || response.statusCode == 201){
+        log("It is working");
+        Get.back();
+        Get.back();
+        AppSnackBar.showSuccess("Service successfully added!!");
+      }
+      else if(response.statusCode == 417){
+        Get.back();
+        AppSnackBar.showError("Check your email to complete onboarding");
+      }
+      else{
+        Get.back();
+        AppSnackBar.showError("The status code is: ${response.statusCode}");
+      }
+
+    }catch(e){
+      log("-----------------I am here in post now catch----------------------");
+      AppSnackBar.showError(e.toString());
     }
   }
 }
