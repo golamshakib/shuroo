e9315124f57e5536e805e7aeba2281fcb7f30707
Nifat130 <mahingreen130@gmail.com>
2025-07-07 12:56:12 +0600

    repost problem solved by Nifat

--- lib/features/post_creation_repost_delete/controller/repost_with_throught_screen_controller.dart
+++ lib/features/post_creation_repost_delete/controller/repost_with_throught_screen_controller.dart
@@ -10,53 +10,80 @@ import 'package:shuroo/features/post_creation_repost_delete/controller/my_all_po
 import 'package:shuroo/features/profile/controller/personal_creation_controller.dart';
 import 'package:shuroo/features/profile/data/user_data_model.dart';
 
+import '../data/single_post_data_model.dart';
+
 class RepostWithThroughtScreenController extends GetxController {
   final TextEditingController textController = TextEditingController();
 
   var isLoading = false.obs;
 
   // Expose controllerOne here as a public variable or getter
-  final MyAllPostScreenController controllerOne = Get.find<MyAllPostScreenController>();
+  // final MyAllPostScreenController controllerOne = Get.find<MyAllPostScreenController>();
 
   var userProfileInfo = GetUser().obs;
-
+  var singlePost = SinglePostDataModel().obs;
   @override
   void onInit() async{
     // TODO: implement onInit
     super.onInit();
     if(Get.arguments != null){
       log("===========================Here in init=================================");
+      log("===========================postId: ${Get.arguments}=================================");
       isLoading.value = true;
       log("===========================Here making true=================================");
-      final object = Get.find<PersonalCreationController>();
-   // await   object.getProfile().then((onValue){
-   //   if(object.profile.data != null){
-   //     log("Data is not nul: ${object.profile.data!.name}");
-   //     userProfileInfo.value = object.profile;
-   //   }
-   //
-   // });
-
-      ever(object.isLoading, (callback){
-        if(object.isLoading.value){
-          log("Loading........");
-        }else
-        {
-          userProfileInfo.value = object.profile;
-        }
-
-      });
+      await  Future.wait([
+        fetchProfile(),
+        getSinglePost(Get.arguments)
+      ]);
+      //await fetchProfile();
 
       log("===========================After fetched Profile=================================");
-      await postRepost(Get.arguments);
+      //await getSinglePost(Get.arguments);
       log("===========================After fetched post=================================");
       isLoading.value = false;
       log("===========================Loader False=================================");
     }
   }
 
-  void fetchPost(String postId) async {
-    await controllerOne.getSinglePost(postId);
+  Future<void> fetchProfile() async{
+    final object = Get.find<PersonalCreationController>();
+    // await   object.getProfile().then((onValue){
+    //   if(object.profile.data != null){
+    //     log("Data is not nul: ${object.profile.data!.name}");
+    //     userProfileInfo.value = object.profile;
+    //   }
+    //
+    // });
+
+    ever(object.isLoading, (callback){
+      if(object.isLoading.value){
+        log("Loading........");
+      }else
+      {
+        userProfileInfo.value = object.profile;
+      }
+
+    });
+  }
+
+  Future<void> getSinglePost(String id) async {
+    try {
+      //  showProgressIndicator();
+      final response =
+      await NetworkCaller().getRequest("${AppUrls.getSinglePost}/$id");
+      if (response.isSuccess && response.statusCode == 200) {
+        final data = response.responseData;
+        singlePost.value = SinglePostDataModel.fromJson(data);
+      } else if (response.statusCode == 404) {
+        AppSnackBar.showError('Data Not Found');
+      } else {
+        AppSnackBar.showError('Something Went Wrong');
+      }
+    } catch (e) {
+      log('Something went Wrong $e');
+    } finally {
+      //  hideProgressIndicator();
+    }
   }
 
   Future<void> postRepost(String postId) async {
